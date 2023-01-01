0ea7de5c6cf24e71fc5edc6eebf59b8947b18e14
sinjy1203 <sinjy1203@naver.com>
2023-01-01 17:41:23 +0900

    calibration

--- draw_line.py
+++ draw_line.py
@@ -2,8 +2,8 @@ import cv2
 import numpy as np
 from pathlib import Path
 
-drawing_fname = Path('./data/drawing_chess/drawing_img2.png')
-raw_fname = Path("./data/undistortion_chess/undistortion_capture0.png")
+drawing_fname = Path('./data/drawing_chess/undistortion_drawing_img2.png')
+raw_fname = Path("./data/undistortion_chess/undistortion_capture1.png")
 
 
 def mouse_event(event, x, y, flags, param):
